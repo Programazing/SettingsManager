cea569f92f87280d7080ddbd83883df337075c39
Programazing <chris@thatamazingprogrammer.com>
2019-12-18 02:16:58 -0500

    Add two new properties

--- SettingsManager/SettingsManager/SettingsModel.cs
+++ SettingsManager/SettingsManager/SettingsModel.cs
@@ -3,5 +3,7 @@
     public class SettingsModel
     {
         public string SomeSetting { get; set; } = "data";
+        public string AnotherSetting { get; set; } = "more data";
+        public int NumberSetting { get; set; } = 42;
     }
 }
